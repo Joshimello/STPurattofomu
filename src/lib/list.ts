3a1ad6fca2116bf91dd054f78f46c6dd620e2997
111000277 <taykhaili277@gapp.nthu.edu.tw>
2024-06-20 15:12:47 +0800

    list done

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -2,5 +2,81 @@ export const list = {
   "ST 2": {
     id: 37606,
     code: "2011-022B",
-  }
+  },
+  "ST 1": {
+    id: 25460,
+    code: "1998-049A",
+  },
+  "IRIS-A": {
+    id: 51044,
+    code: "2022-002BN",
+  },
+  "IDEASSAT": {
+    id: 47458,
+    code: "2021-006AX",
+  },
+  "YUSAT-1": {
+    id: 47439,
+    code: "2021-006AC",
+  },
+  "FORMOSAT7-5/COSMIC2-5": {
+    id: 44358,
+    code: "2019-036V",
+  },
+  "FORMOSAT7-6/COSMIC2-6": {
+    id: 44353,
+    code: "2019-036Q",
+  },
+  "FORMOSAT7-2/COSMIC2-2": {
+    id: 44351,
+    code: "2019-036N",
+  },
+  "FORMOSAT7-4/COSMIC2-4": {
+    id: 44350,
+    code: "2019-036M",
+  },
+  "FORMOSAT7-1/COSMIC2-1": {
+    id: 44349,
+    code: "2019-036L",
+  },
+  "FORMOSAT7-3/COSMIC2-3": {
+    id: 44343,
+    code: "2019-036E",
+  },
+  "FORMOSAT-5": {
+    id: 42920,
+    code: "2017-049A",
+  },
+  "PACE": {
+    id: 40022,
+    code: "2014-033N",
+  },
+  "FORMOSAT 3F": {
+    id: 29052,
+    code: "2006-011F",
+  },
+  "FORMOSAT 3E": {
+    id: 29051,
+    code: "2006-011E",
+  },
+  "FORMOSAT 3D": {
+    id: 29050,
+    code: "2006-011D",
+  },
+  "FORMOSAT 3C": {
+    id: 29049,
+    code: "2006-011C",
+  },
+  "FORMOSAT 3B": {
+    id: 29048,
+    code: "2006-011B",
+  },
+  "FORMOSAT 3A": {
+    id: 29047,
+    code: "2006-011A",
+  },
+  "ROCSAT 2": {
+    id: 28254,
+    code: "2004-018A",
+  },
 }
